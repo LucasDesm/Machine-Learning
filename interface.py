ff118cf6209440a713b606a1cbf09c2d913d7b35
Baptiste Desfontaines <baptistedesfontaines@pm.me>
2026-04-28 23:30:20 +0200

    Add train and predict to interface. Need to find a way to use labelencoder on input values with same logic as dataseet precessing

--- interface.py
+++ interface.py
@@ -1,8 +1,62 @@
+#%% ---------------------------------------------------------------------------------------------------
 from flask import Flask, render_template, request
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from imblearn.over_sampling import SMOTE
+from sklearn.pipeline import Pipeline
 
+#%% ---------------------------------------------------------------------------------------------------
+# Importer datas
+df=pd.read_csv('heart_disease_uci.csv')
+
+#%% ---------------------------------------------------------------------------------------------------
+# Traiter dataset
+df = df.drop('id', axis=1)
+
+df['chol'] = df['chol'].replace(0, np.nan)
+df['trestbps'] = df['trestbps'].replace(0, np.nan)
+df.loc[df['oldpeak'] < 0, 'oldpeak'] = np.nan
+
+colonnes_num = ['age', 'trestbps', 'chol', 'thalch', 'oldpeak', 'ca']
+for col in colonnes_num : 
+    df[col] = df[col].fillna(df[col].mean())
+    
+colonnes_cat = ['sex','dataset','cp','fbs', 'restecg', 'exang', 'slope', 'thal']
+
+for col in colonnes_cat :
+    df[col] = df[col].fillna(df[col].mode()[0])
+
+def categorical_features(df):
+    object_columns = df.select_dtypes(include=['object','bool']).columns
+    label_encoder = LabelEncoder()
+    for col in object_columns:
+        df[col] = label_encoder.fit_transform(df[col].astype(str))
+    return df
+
+df = categorical_features(df)
+
+X = df.drop('num',axis=1)
+Y=df['num']
+
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size=0.25, random_state=123)
+
+#%% ---------------------------------------------------------------------------------------------------
+# Entrainement
+smote = SMOTE(random_state=42)
+X_train_res, Y_train_res = smote.fit_resample(X_train, Y_train)
+
+steps = [('scaler',StandardScaler()),('model', DecisionTreeClassifier())]
+
+p1 = Pipeline(steps)
+p1.fit(X_train_res, Y_train_res)
+print(p1.score(X_test, Y_test))
+
+#%% ---------------------------------------------------------------------------------------------------
+# Créer app flask avec 2 routes
 # Tuto suivi: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -30,6 +84,7 @@ def traitement():
     input_data = pd.DataFrame({
         'age': [age],
         'sex': [sex],
+        'dataset': [''],
         'cp': [cp],
         'trestbps': [trestbps],
         'chol': [chol],
@@ -42,9 +97,23 @@ def traitement():
         'ca': [ca],
         'thal': [thal]
     })
+    input_data = input_data.astype({
+        'age': int,
+        'trestbps': int,
+        'chol': int,
+        'thalch': int,
+        'oldpeak': float,
+        'ca': int,
+    })
+    input_data = categorical_features(input_data)
+    Y_pred = p1.predict(input_data)
 
-    result = "ok"
+    result = Y_pred[0]
     if result :
         return render_template("traitement.html", input_data = input_data, result = result)
     else:
         return render_template("traitement.html")
+#%% ---------------------------------------------------------------------------------------------------
+# Lance serveur flask
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5005, debug=True, use_reloader=False)
